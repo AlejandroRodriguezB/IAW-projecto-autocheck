68de3351d31df9ff48bb3d37c9798e18f786fe5c
Arbnoob <iesalejandrorodriguezbanon@gmail.com>
2018-10-04 17:52:23 +0200

    java run run

--- src/main/java/com/iesemilidarder/base/Launcher.java
+++ src/main/java/com/iesemilidarder/base/Launcher.java
@@ -1,4 +1,13 @@
 package com.iesemilidarder.base;
 
+import com.iesemilidarder.base.data.Moto;
+
 public class Launcher {
+    public static void main(String... args) {
+        System.out.println("Concesionario forest run app 70.0");
+        Moto moto = new Moto();
+        moto.start();
+        System.out.println("let's go boizs");
+        moto.brake();
+    }
 }
---
+++ src/main/java/com/iesemilidarder/base/data/Vehicle.java
@@ -0,0 +1,8 @@
+package com.iesemilidarder.base.data;
+
+public abstract class Vehicle {
+    public abstract void start();
+    public void brake(){
+        System.out.println("freno");
+    }
+}
